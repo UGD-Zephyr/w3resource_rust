9e56daa364e74b5fb4f5145b52ea29fada388010
Zephyr <ugd.zephyr@gmail.com>
2023-04-25 20:56:51 +0200

    New commit

--- array/array_001.rs
+++ array/array_001.rs
@@ -6,4 +6,21 @@
 
 fn main(){ 
 
+    let array1: [i32; 5] = [1, 2, 3, 4, 5];
+    let mut total_loops: usize = 0;
+
+    /*
+     * Other valid options for printing an array on the screen.
+     *
+    println!("{:?}", array1);
+    println!("{:#?}", array1);
+    */
+
+        for index in 0..array1.len(){
+            print!("{} ", array1[index]);
+            total_loops = index + 1;
+        }
+        println!("Total loops: {}", total_loops);
+        println!();
+
 } 
